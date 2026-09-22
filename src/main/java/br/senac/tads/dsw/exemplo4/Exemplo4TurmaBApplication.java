830c7e2a54283dbbf1bca866896f23452a8bf8f5
VINICIUS DE SOUZA VIEIRA <vinicius.svieira12@senacsp.edu.br>
2026-09-22 08:49:19 -0300

    inicial

---
+++ src/main/java/br/senac/tads/dsw/exemplo4/Exemplo4TurmaBApplication.java
@@ -0,0 +1,13 @@
+package br.senac.tads.dsw.exemplo4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exemplo4TurmaBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exemplo4TurmaBApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/senac/tads/dsw/exemplo4/Exemplo4TurmaBApplicationTests.java
@@ -0,0 +1,13 @@
+package br.senac.tads.dsw.exemplo4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exemplo4TurmaBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
